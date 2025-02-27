7baad5528282eeb40938ee0e03a22d252d841ec9
Rodrigo Montelongo <rodrigo00mp@gmail.com>
2025-02-26 18:58:20 -0600

    First look at dashboard

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
+
 
 
 
@@ -9,4 +11,26 @@ st.title('E-Commerce Sales Dashboard')
 
 df = pd.read_csv('ecomm_sales_data.csv')
 
-df
+#Convert Order Date to datetime format
+df['Order Date'] =  pd.to_datetime(df['Order Date'])
+
+def sales_by_month():
+
+  #Sort values by Order Date in descending order
+  sortedDf = df.sort_values(by='Order Date').reset_index()
+  sortedDf['year-month'] = sortedDf['Order Date'].dt.to_period('M').astype(str)
+  grouping = sortedDf.groupby(sortedDf['year-month'])['Sales'].sum().reset_index()
+  return grouping
+  
+monthly_sales = sales_by_month()
+
+fig = px.bar(monthly_sales, x='year-month', y='Sales',
+             labels={'year_month': 'Month', 'sales': 'Total Sales'},
+             title='Monthly Sales')
+
+  # Display in Streamlit
+  
+st.plotly_chart(fig)
+
+
+
