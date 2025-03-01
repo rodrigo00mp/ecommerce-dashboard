1bcaf1f65d055999c82e5eb11d5cdc159d4d9b24
Rodrigo Montelongo <rodrigo00mp@gmail.com>
2025-03-01 02:20:05 -0600

    Added sidebar and top products sold

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 import plotly.express as px
 from millify import millify 
-
+import us
 
 
 st.set_page_config(layout='wide')
@@ -12,83 +13,99 @@ st.title('E-Commerce Sales Dashboard')
 
 ### -------- Functions --------- ####
 
+
 @st.cache_data
 def load_data(path:str):
   data = pd.read_csv('ecomm_sales_data.csv')
+  data['Order Date'] =  pd.to_datetime(data['Order Date'])
+  data['Ship Date'] =  pd.to_datetime(data['Ship Date'])
+  data['Year-month'] = data['Order Date'].dt.to_period('M').astype(str)
+  data['Year'] = data['Order Date'].dt.to_period('Y').astype(str)
   return data
 
 
 #Monthly Sales Chart#
 
-def sales_by_month():
+def sales_per_month():
   #Sort values by Order Date in descending order
   sortedDf = df.sort_values(by='Order Date').reset_index()
 
-  #Create new column to uniquely identif month. Conver to string so plotly doesn`t crash
-  sortedDf['year-month'] = sortedDf['Order Date'].dt.to_period('M').astype(str)
-
   #Group by month and sum Sales
-  grouping = sortedDf.groupby(sortedDf['year-month'])['Sales'].sum().reset_index()
-  return grouping
+  grouping = sortedDf.groupby(sortedDf['Year-month'])['Sales'].sum().reset_index()
 
-def sales_by_year():
+  #Create plotly figure
+  fig = px.bar(grouping, x='Year-month', y='Sales',
+              labels={'Year-month': 'Month', 'Sales': 'Total Sales'},
+             title='Sales per month')
+  st.plotly_chart(fig, use_container_width=True, config={'displayModeBar': False})
+
+
+def sales_per_year():
   #Sort values by Order Date in descending order
   sortedDf = df.sort_values(by='Order Date').reset_index()
 
-  #Create new column to uniquely identif month. Conver to string so plotly doesn`t crash
-  sortedDf['year'] = sortedDf['Order Date'].dt.to_period('Y').astype(str)
 
   #Group by month and sum Sales
-  grouping = sortedDf.groupby(sortedDf['year'])['Sales'].sum().reset_index()
-  return grouping
+  grouping = sortedDf.groupby(sortedDf['Year'])['Sales'].sum().reset_index()
+  fig = px.bar(grouping, x='Year', y='Sales',
+              labels={'Year': 'Year', 'Sales': 'Total Sales'},
+             title='Sales per Year')
+  st.plotly_chart(fig, use_container_width=True,config={'displayModeBar': False})
 
+def get_top_products():
+  df_products = df.copy()
+  top_products = df_products.groupby('Product ID')['Profit'].sum().sort_values(ascending=True).reset_index()
+  fig = px.bar(top_products.tail(10), x='Profit', y='Product ID')
+  st.plotly_chart(fig, use_container_width=True, config={'displayModeBar': False})
 
 # --------------------------------------- #
 
-
 df = load_data('ecomm_sales_data.csv')
  #Convert Order Date to datetime format
-df['Order Date'] =  pd.to_datetime(df['Order Date'])
+with st.expander('Sales Data Preview'):
+  st.dataframe(df, column_config={'Order Date': st.column_config.DateColumn(format="DD/MM/YYYY"), 'Ship Date': st.column_config.DateColumn(format="DD/MM/YYYY")})
 # Metrics
 
 orders =df['Order ID'].nunique()
 aov = df.groupby(df['Order ID'])['Sales'].sum().mean()
 sales = df['Sales'].sum()
 
-col1, col2, col3 = st.columns(3)
-col1.metric("Orders", millify(orders), "20%", border=True)
-col2.metric("Sales", "$" + millify(sales), "-3%", border=True)
-col3.metric("Average Order Value", "$" + millify(aov, precision=2), "4%", border=True)
-
-
-
- 
-
 # Display in Streamlit
 
-# Monthly Sales
-monthly_sales = sales_by_month()
-    # Monthly Sales
-yearly_sales = sales_by_year()
+met1, met2, met3 = st.columns(3)
 
+with met1:
+  st.metric('Total Orders', orders, border=True)
 
-st.subheader("Sales")
-sales_dropdown = st.selectbox("Sales by:", ('Month', 'Year'), label_visibility='collapsed')
+with met2:  
+  st.metric('Total Sales', millify(sales), border=True)
 
-if sales_dropdown == 'Month':
-  
+with met3:
+  st.metric('Average Order Value', millify(aov), border=True)
+
+with st.sidebar:
+  sales_dropdown = st.selectbox("Year", np.sort(df['Year'].unique()), label_visibility='collapsed')
+
+top_left, top_right = st.columns([0.7,0.3])
+with top_left:
+  st.subheader("Sales")
+  if sales_dropdown == 'Month':
+    #Monthly Sales
+    sales_per_month()
+  else:
+    sales_per_year()
+
+with top_right:
+    st.markdown("### Top 10 Products ###")
+    get_top_products()
+    
+#else:
+
   #Create plotly figure
-  fig = px.bar(monthly_sales, x='year-month', y='Sales',
-              labels={'year-month': 'Month', 'Sales': 'Total Sales'},
-             title='Sales per month')
-  st.plotly_chart(fig, config={'displayModeBar': False})
-else:
-
-  #Create plotly figure
-  fig = px.bar(yearly_sales, x='year', y='Sales',
-              labels={'year': 'Month', 'Sales': 'Total Sales'},
-             title='Sales per Year')
-  st.plotly_chart(fig, config={'displayModeBar': False})
+  #fig = px.bar(yearly_sales, x='year', y='Sales',
+              #labels={'year': 'Month', 'Sales': 'Total Sales'},
+             #title='Sales per Year')
+  #st.plotly_chart(fig, config={'displayModeBar': False})
 
 
 
